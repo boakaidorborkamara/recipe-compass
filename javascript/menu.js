1137a5a966eda0e94f2037cf553eec8e8c2050e9
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-03-10 10:28:32 +0000

    js files for menu and home page modified

--- javascript/menu.js
+++ javascript/menu.js
@@ -5,22 +5,17 @@
     fetch('https://forkify-api.herokuapp.com/api/search?q=pizza')
     .then(res => res.json())
     .then((data)=>{
-        // console.log(data);
         let all_details = data;
-        console.log(all_details);
         let all_data_values = Object.values(all_details.recipes);
-        console.log(all_data_values);
         
         all_data_values.forEach(ele => {
             //creating new html element for each element
             let dish = document.createElement('div');
             let dish_image = document.createElement('div');
             let image = document.createElement('img');
-            
-
-
             let dish_detail = document.createElement('div');
 
+            // child elements for dish detail
             let title = document.createElement('p');
             let title_text = document.createTextNode(ele.title);
             title.appendChild(title_text);
@@ -33,8 +28,6 @@
             let id_text = document.createTextNode(ele.recipe_id);
             id.appendChild(id_text);
 
-
-
             //adding content to each element created by appending child
             dish_image.appendChild(image);
             dish.appendChild(dish_image);
@@ -45,10 +38,6 @@
             dish_detail.appendChild(publisher);
             dish_detail.appendChild(id);
 
-            
-           
-           
-
             //add class to indivdual element created by JS to enable CSS styling to take effect
             dish.setAttribute('class', 'dish');
             dish_image.setAttribute('class', 'dish_image');
@@ -58,13 +47,13 @@
            
         });
 
+        // display an image in the showcase section
         let showcase_image = document.createElement('img');
             showcase_image.setAttribute('src', all_data_values[26].image_url);
             showcase_image.setAttribute('class','large_product_image')
             showcase_area.appendChild(showcase_image);
-        
-           
-    
-      
+    })
+    .catch(err=>{
+        console.log(err);
     })
    
