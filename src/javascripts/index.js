db3af90871145aa522b0e02314bad3dcc6a6cd1b
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-11-25 00:23:14 +0000

    style home page

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,3 +1,4 @@
 import { names } from "./names";
 import "../css/style.css"
+// import {food1} from
 console.log("working", names);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  mode: "development",
+  entry: "./src/javascripts/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
