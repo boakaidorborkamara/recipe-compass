0eb62e22f5245115c844320b9e565232fe1427d3
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2023-11-29 11:50:55 +0000

    refactor the fetchRecipes function to use async await try and catch blocks for error handling

--- src/javascripts/util/fetchRecipes.js
+++ src/javascripts/util/fetchRecipes.js
@@ -1,6 +1,10 @@
-export const fetchRecipes = async (url)=>{
+export const fetchRecipes = async (url) => {
+  try {
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
     return data;
-}
+  } catch (err) {
+    console.log(err);
+  }
+};
